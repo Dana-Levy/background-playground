738822cc86b263cd01d17e12167faa2cdde8e0f3
Codux <codux@codux.com>
2024-07-24 12:04:23 +0300

    test of background

---
+++ src/_codux/boards/background-color/background-color.board.tsx
@@ -0,0 +1,13 @@
+import { createBoard } from '@wixc3/react-board';
+import { BackgroundColor } from '../../../components/background-color/background-color';
+import styles from './background-color.board.module.scss';
+
+export default createBoard({
+    name: 'Background-Color',
+    Board: () => <BackgroundColor className={styles.backgroundColor} />,
+    isSnippet: true,
+    environmentProps: {
+        windowWidth: 1716,
+        windowHeight: 1410,
+    },
+});
